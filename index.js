eb5ba407e14497f8c3a8de1b38f2b43a8b4ae865
Hugo Müller-Downing <hugo@localz.co>
2017-12-05 18:26:19 +1100

    Add terrible code that somehow works ✨

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('mz/fs');
+const http = require('http');
+const {Readable} = require('stream');
+const colors = require('colors/safe');
+
+const frames = [];
+
+// Setup frames in memory
+fs.readdir('./frames').then(data => { 
+  data.forEach(async frame => {
+    const f = await fs.readFile(`./frames/${frame}`);
+    frames.push(f.toString());
+  })
+});
+
+const colorsOptions = ['red', 'yellow', 'green', 'blue', 'magenta', 'cyan', 'white'];
+
+const streamer = stream => {
+  let index = 0;
+  setInterval(() => {
+    if (index >= frames.length) index = 0; stream.push('\033c');
+    const c = colorsOptions[Math.floor(Math.random() * colorsOptions.length)];
+    stream.push(colors[c](frames[index]));
+    stream.push('\n')
+    index++;
+  }, 70);
+}
+
+const server = http.createServer((req, res) => {
+  if (!req.headers['user-agent'].includes('curl')) {
+    res.writeHead(302, {'Location': 'https://twitter.com/hugojmd'});
+    return res.end();
+  }
+  const stream = new Readable();
+  stream._read = function noop () {};
+  stream.pipe(res);
+  streamer(stream);
+});
+
+
+server.listen(3000, err => {
+  if (err) throw err;
+  console.log('Listening on locahost:3000');
+});
