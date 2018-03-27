b21719862a203b1f4a39833567087a057df7fdd7
Steve Franchak <stevefranchak@gmail.com>
2018-03-27 13:56:41 -0400

    Rewrote parrot.ps1 to actually use the parrot.live http server. Renamed to parrot-dance.ps1.

--- parrot.ps1
+++
@@ -1,4 +0,0 @@
-# a quick powershell one-liner so our windows friends can also enjoy terminal parrots!
-# Invoke-WebRequest and Invoke-RestMethod expect the stream to end and don't currently 
-# support displaying anything prior to that.
-$urlstring="https://raw.githubusercontent.com/hugomd/parrot.live/master/frames/"; while (1) {foreach ($i in 1..9) {[Console]::ForegroundColor = [System.ConsoleColor](Get-Random -Min 0 -Max ([System.ConsoleColor].GetFields().Count - 1)); (Invoke-WebRequest $($urlstring+$i+".txt")).content; start-sleep -milliseconds 70}}
