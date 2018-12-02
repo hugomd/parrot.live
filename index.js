371bcfcdb1e063484cbd0fcf7b7726ec025ba95a
Phred <fearphage@gmail.com>
2018-12-02 11:12:09 -0600

    simplified code
    * Mostly implementing suggestinos left for #6

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const { Readable } = require('stream');
 const colors = require('colors/safe');
 
 // Setup frames in memory
-let frames;
+let original;
 let flipped;
 
 (async () => {
   const framesPath = 'frames';
   const files = await fs.readdir(framesPath);
 
-  frames = await Promise.all(files.map(async (file) => {
+  original = await Promise.all(files.map(async (file) => {
     const frame = await fs.readFile(path.join(framesPath, file));
     return frame.toString();
   }));
-  flipped = frames.map(f => {
+  flipped = original.map(f => {
     return f
       .toString()
       .split('')
@@ -39,43 +39,37 @@ const colorsOptions = [
   'white'
 ];
 const numColors = colorsOptions.length;
+const selectColor = previousColor => {
+  let color;
+
+  do {
+    color = Math.floor(Math.random() * numColors);
+  } while (color === previousColor);
+
+  return color;
+};
 
 const streamer = (stream, opts) => {
-  const { flip } = opts;
-
   let index = 0;
-  let lastColor = -1;
-  let newColor = 0;
+  let lastColor;
   let frame = null;
+  const frames = opts.flip ? flipped : original;
 
   return setInterval(() => {
-    if (index >= frames.length) index = 0;
-    if (index < 0) index = frames.length - 1;
-    
     // clear the screen
     stream.push('\033[2J\033[H');
-    
-    newColor = Math.floor(Math.random() * numColors);
 
-    // Reroll for a new color if it was the same as last frame
-    if (newColor == lastColor) {
-      newColor += 1 + Math.floor(Math.random() * (numColors - 1));
-      newColor %= numColors;
-    }
+    const newColor = lastColor = selectColor(lastColor);
 
-    lastColor = newColor;
-    frame = flip ? flipped[index] : frames[index];
-    stream.push(colors[colorsOptions[newColor]](frame));
-    index++;
+    stream.push(colors[colorsOptions[newColor]](frames[index]));
+
+    index = (index + 1) % frames.length;
   }, 70);
 };
 
-const validateQuery = query => {
-  const { flip } = query;
-  return {
-    flip: typeof flip === 'string' && flip.toLowerCase() == 'true'
-  };
-};
+const validateQuery = ({ flip }) => ({
+  flip: String(flip).toLowerCase() === 'true'
+});
 
 const server = http.createServer((req, res) => {
   if (
@@ -86,11 +80,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(302, { Location: 'https://github.com/hugomd/parrot.live' });
     return res.end();
   }
-  const { query } = url.parse(req.url, true);
   const stream = new Readable();
   stream._read = function noop() {};
   stream.pipe(res);
-  const interval = streamer(stream, validateQuery(query));
+  const interval = streamer(stream, validateQuery(url.parse(req.url, true).query));
 
   req.on('close', () => {
     stream.destroy();
